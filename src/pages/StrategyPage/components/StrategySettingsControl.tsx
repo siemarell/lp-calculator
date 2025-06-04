c32971006d7b6183f568f3b62ed6d933b100da1b
siem <siemarell@gmail.com>
2025-06-04 18:43:27 +0300

    *

---
+++ src/pages/StrategyPage/components/StrategySettingsControl.tsx
@@ -0,0 +1,70 @@
+import cn from "classnames";
+import { observer } from "mobx-react-lite";
+import { Strategy } from "src/strategy/strategy";
+import { Block } from "src/components/Block";
+import {
+  TextField,
+  InputAdornment,
+  Typography,
+  Grid,
+} from "@mui/material";
+
+export const StrategySettingsControl = observer(
+  (props: { strategy: Strategy; className?: string }) => {
+    return (
+      <Block className={cn("flex flex-col gap-4", props.className)}>
+        <Typography variant="h6">Strategy Settings</Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={4}>
+            <TextField
+              label="Days In Position"
+              type="number"
+              InputProps={{
+                inputProps: { step: 1, min: 1 },
+                endAdornment: <InputAdornment position="end">days</InputAdornment>,
+              }}
+              size="small"
+              fullWidth
+              value={props.strategy.daysInPosition}
+              onChange={(e) => {
+                props.strategy.daysInPosition = Number(e.target.value);
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <TextField
+              label="Chart Min Price"
+              type="number"
+              InputProps={{
+                inputProps: { step: 100 },
+              }}
+              size="small"
+              fullWidth
+              value={props.strategy.minPrice}
+              onChange={(e) => {
+                props.strategy.minPrice = Number(e.target.value);
+                props.strategy.updatePrices();
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <TextField
+              label="Chart Max Price"
+              type="number"
+              InputProps={{
+                inputProps: { step: 100 },
+              }}
+              size="small"
+              fullWidth
+              value={props.strategy.maxPrice}
+              onChange={(e) => {
+                props.strategy.maxPrice = Number(e.target.value);
+                props.strategy.updatePrices();
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Block>
+    );
+  },
+);
