f5d195d6614aad2e3d96c0b7c2f02bd772cecf58
siem <siemarell@gmail.com>
2025-06-21 11:23:01 +0300

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { StrategyPage } from "src/pages/StrategyPage/StrategyPage";
 
 function App() {
   return (
-    <div className="App">
-      <StrategyPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<StrategyPage />} />
+          {/* Add more routes here as needed */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
